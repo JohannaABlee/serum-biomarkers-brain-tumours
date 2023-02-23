b52883fe8d20f46fe48107d14413b1f946896f29
JohannaABlee <99752270+JohannaABlee@users.noreply.github.com>
2023-02-23 16:51:49 +0000

    Update GFAP_levels_GBM.m

--- GFAP_levels_GBM.m
+++ GFAP_levels_GBM.m
@@ -2,55 +2,55 @@
 %Johanna Blee 04/04/2021
 
 %-----parameter intialisation ----
-time=linspace(1,1000,1000);%time of growth simulation
+time=linspace(1,600,600);%time of growth simulation
 pt=5714*10^4;%tumour cell density (cells/ml)
 pn=4800*10^4;%necrotic cell density (cells/ml)
 N0=1; %intial number of tumour cells 
 V0=1/pt; %intial volume of tumour cells
-Nn0=1;
-Vn0=1/pn;
+Vp=4500; %ml mean plasma volume
+cthr=0.12;%current ut-off limit
+CPss=0.07; %ng/ml average conc in serum of healthy patients
+KHUH=CPss*(yp*Vp); %average production*fraction in healthy patients
+qt0=CPss*Vp%initial value in healthy induvidual at tumour onset
+yp=0.7;%days-1 decay rate in serum
+
+
 
 a=0.008;%growth rate of tumour 
-b=0.01;%growth rate of necrotic regions
+b=0.009;%growth rate of necrotic regions
 maxVt=158;%maximum tumour volume (ml)
-maxNt=158*pt; %maximum number of tumour cells
-
-Vt=volT(time,maxVt,a,V0); %tumour vol ml 
-Nt=volT(time,maxNt,a,N0); %number of tumour cells
+maxNt=maxVt*pt; %maximum number of tumour cells
+maxVn=150;%maximum necrotic volume
+maxNn=pn*maxVn;%maximum number of necrotic cells
 
 vn0=0.5;% volume of onset of necoris ml (2cm^3);
 nn0=vn0*pt; %number of tumour cells at onset of necrosis 
 [minValue,closestIndex]=min(abs(Vt-vn0));
 tn0=time(closestIndex); %time onset necrosis
 
-maxVn=150;%maximum necrotic volume
-maxNn=pn*maxVn;%maximum number of necrotic cells
-
-Vn=voln(tn0,time,maxVn,b,Vn0); %necrotic volume
-Nn=voln(tn0,time,maxNn,b,Nn0); %number of necrotic cells
-yp=0.69;%days-1 decay rate in serum
+%necrosis constants
+Rd=b;%rate of cell death- same as necrosis growth rate
+Qn=3.1*10^-4;%average level of GFAP in necrotic cells
 
 %kfunction intilaisation
 tk0=tn0%inthis scenario assume necrosis and k onset align
-Ktmin=0.05;%minimum k value
-Ktmax=0.8;%maximum k value
+Ktmin=0;%minimum k value
+Ktmax=0.5;%maximum k value
 h=7;%rate of k change hill slope constant
-Ktt=100+tk0;%half time - when k is half max 
+Ktt=225;%half time - when k is half max 
 
 
-%necrosis constants
-Rd=b;%rate of cell death- same as necrosis growth rate
-Qn=3*10^-4;%average level of GFAP in necrotic cells
+%growth of tumour and necrosis
+Vt=volT(time,maxVt,a,V0); %tumour vol ml 
+Nt=volT(time,maxNt,a,N0); %number of tumour cells
 
-Vp=4500; %ml mean plasma volume
+Vn=voln(tn0,time,maxVn,b,Vn0); %necrotic volume
+Nn=voln(tn0,time,maxNn,b,Nn0); %number of necrotic cells
 
 
-CPss=0.07; %ng/ml average conc in serum of healthy patients
-KHUH=CPss*(yp*Vp); %average production*fraction in healthy patients
-qt0=CPss*Vp%initial value in healthy induvidual at tumour onset
 
 %----ODE to give GFAP serum levels as a function of time ----
-[time2,ytn1]=ode45(@(time2,ytn1)((kfunc(tk0,time2,Ktmin,Ktmax,Ktt,h)*Un(Qn,Rd,tn0,time2,maxNn,b,Nn0))+KHUH-ytn1*yp),totaltime,qt0);
+[time2,ytn1]=ode45(@(time2,ytn1)((kfunc(tk0,time2,Ktmin,Ktmax,Ktt,h)*Un(Qn,Rd,tn0,time2,maxNn,b,Nn0))+KHUH-ytn1*yp),time,qt0);
 CP1=ytn1/Vp; %concentration of GFAP with time
 vol2=volT(time2,maxVt,a,V0); %tumour volume at a given time point
 
@@ -72,3 +72,6 @@ figure
 plot(time2,CP1)
 ylabel('Serum GFAP (ng/ml)')
 xlabel('Time (days)')
+
+disp(['current detection time=', num2str(tthresh1)]);
+disp(['current detection volume=', num2str(vthresh1)]);
